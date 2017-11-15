1d05f23cba8173d4c0f7d68aef405a7eed61204f
Mechiel Lukkien <m.lukkien@irias.nl>
2017-11-15 18:42:35 +0100

    implement the status() sherpa api call

--- api.go
+++ api.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"time"
 
 	"bitbucket.org/mjl/sherpa"
 )
@@ -29,6 +30,92 @@ type Ding struct {
 // Checks program status.
 // If backend connectivity is broken, this sherpa call results in a 500 internal server error. Useful for monitoring tools.
 func (Ding) Status() {
+	type what int
+	const (
+		filesystem what = iota
+		xdatabase
+		timer
+	)
+
+	type done struct {
+		what  what
+		error bool
+	}
+
+	errors := make(chan done, 3)
+
+	go func() {
+		defer os.Remove("data/test")
+		f, err := os.Create("data/test")
+		if err == nil {
+			err = f.Close()
+		}
+		if err != nil {
+			log.Printf("status: file system unavailable: %s\n", err)
+			errors <- done{filesystem, true}
+			return
+		}
+		errors <- done{filesystem, false}
+	}()
+
+	go func() {
+		var one int
+		err := database.QueryRow("select 1").Scan(&one)
+		if err != nil {
+			log.Printf("status: database unavailable: %s\n", err)
+			errors <- done{xdatabase, true}
+			return
+		}
+		errors <- done{xdatabase, false}
+	}()
+
+	timeout := time.AfterFunc(time.Second*5, func() {
+		log.Println("status: timeout for db or fs checks")
+		errors <- done{timer, true}
+	})
+
+	statusError := func(msg string) {
+		log.Println("status:", msg)
+		panic(&sherpa.InternalServerError{"serverError", msg})
+	}
+
+	db := false
+	fs := false
+	for !db || !fs {
+		done := <-errors
+		if !done.error {
+			switch done.what {
+			case filesystem:
+				fs = true
+			case xdatabase:
+				db = true
+			default:
+				serverError("status: internal error")
+			}
+			continue
+		}
+
+		timeout.Stop()
+		switch done.what {
+		case filesystem:
+			statusError("filesystem unavailable")
+		case xdatabase:
+			statusError("database unavailable")
+		case timer:
+			if !db && !fs {
+				statusError("timeout for both filesystem and database")
+			}
+			if !db {
+				statusError("timeout for database")
+			}
+			if !fs {
+				statusError("timeout for filesystem")
+			}
+		default:
+			serverError("status: missing case")
+		}
+	}
+	timeout.Stop()
 }
 
 func _repo(tx *sql.Tx, repoName string) (r Repo) {
