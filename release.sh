5a69c2aea0e11b0b0e5d310a887c86d4dd2c0758
Mechiel Lukkien <m.lukkien@irias.nl>
2017-11-16 10:11:11 +0100

    make it possible to make windows binaries

--- release.sh
+++ release.sh
@@ -7,9 +7,13 @@ VERSION=$(git describe --tags | sed 's/^v//') \
 GOVERSION=$(go version | cut -f3 -d' ') \
 sh -c '
 	CGO_ENABLED=0;
-	DEST=local/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0};
+	SUFFIX=""
+	if test $GOOS = windows; then
+		SUFFIX=.exe
+	fi
+	DEST=local/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0}${SUFFIX};
 	go build -ldflags "-X main.version=${VERSION:-x}" &&
-	mv $NAME $DEST &&
+	mv ${NAME}${SUFFIX} $DEST &&
 	sh -c "cat assets.zip >>$DEST" &&
 	echo release: $NAME $VERSION $GOOS $GOARCH $GOVERSION $DEST
 '
@@ -20,8 +24,12 @@ VERSION=$(git describe --tags | sed 's/^v//') \
 GOVERSION=$(go version | cut -f3 -d' ') \
 sh -c '
 	CGO_ENABLED=0;
-	DEST=$PWD/local/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0};
+	SUFFIX=""
+	if test $GOOS = windows; then
+		SUFFIX=.exe
+	fi
+	DEST=$PWD/local/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0}${SUFFIX};
 	(cd cmd/dingkick && go build -ldflags "-X main.version=${VERSION:-x}") &&
-	mv cmd/dingkick/$NAME $DEST &&
+	mv cmd/dingkick/${NAME}${SUFFIX} $DEST &&
 	echo release: $NAME $VERSION $GOOS $GOARCH $GOVERSION $DEST
 '
